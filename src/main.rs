cdee5c19e8a874dc65d8aca50fb8bbc62cf7d53c
mascara <hxllfxres@gmail.com>
2025-05-13 20:54:20 +0100

    added button configuration; todo: fix panic error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use discord_rich_presence::activity::{ActivityType, Assets};
+use discord_rich_presence::activity::{ActivityType, Assets, Button as Temp};
 use discord_rich_presence::{DiscordIpc, DiscordIpcClient, activity};
 use iced::widget::{Column, Renderer, Row, button, column, container, row, text, text_input};
 use iced::{Element, Length, Theme};
 use iced_aw::{DropDown, drop_down};
 use std::fmt::Display;
+use std::ops::{Index, IndexMut};
 
 type AppResult<T> = Result<T, Box<dyn std::error::Error>>;
+type Button = discord_rich_presence::activity::Button<'static>;
 
 #[derive(Debug, Clone, Default)]
 struct Asset {
@@ -15,11 +17,18 @@ struct Asset {
     small_text: String,
 }
 
+#[derive(Debug, Clone, Default)]
+struct ActivityButton {
+    label: String,
+    url: String,
+}
+
 #[derive(Debug, Clone)]
 enum Message {
     ChangeState(String),
     ChangeDetails(String),
     ChangeAssets(String, String),
+    ChangeButtons(String, String),
     Select(ActivityTypeChoice),
     Dismiss,
     Expand,
@@ -52,6 +61,19 @@ impl From<ActivityTypeChoice> for ActivityType {
     }
 }
 
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            buttons: Vec::with_capacity(2),
+            state: String::new(),
+            details: String::new(),
+            assets: Asset::default(),
+            selected: ActivityTypeChoice::default(),
+            expanded: false,
+        }
+    }
+}
+
 const CHOICES: [ActivityTypeChoice; 4] = [
     ActivityTypeChoice::Playing,
     ActivityTypeChoice::Listening,
@@ -59,11 +81,11 @@ const CHOICES: [ActivityTypeChoice; 4] = [
     ActivityTypeChoice::Competing,
 ];
 
-#[derive(Default)]
 struct App {
     state: String,
     details: String,
     assets: Asset,
+    buttons: Vec<ActivityButton>,
     selected: ActivityTypeChoice,
     expanded: bool,
 }
@@ -92,9 +114,8 @@ impl App {
         let column = column![
             text_input("Title (State)", &self.state).on_input(Message::ChangeState),
             text_input("Details", &self.details).on_input(Message::ChangeDetails),
-            // open alternate asset menu
-            button(text("Start IPC Server")).on_press(Message::Start),
             drop_down,
+            // Assets
             text_input("Large Image Asset Name", &self.assets.clone().large_image)
                 .on_input(|c| Message::ChangeAssets("large_image".to_string(), c)),
             text_input("Small Image Asset Name", &self.assets.clone().small_image)
@@ -102,10 +123,15 @@ impl App {
             text_input("Large text", &self.assets.clone().large_text)
                 .on_input(|c| Message::ChangeAssets("large_text".to_string(), c)),
             text_input("Small text", &self.assets.clone().small_text)
-                .on_input(|c| Message::ChangeAssets("small_text".to_string(), c))
+                .on_input(|c| Message::ChangeAssets("small_text".to_string(), c)),
+            // buttons
+            text_input("Button 1 Label", &self.buttons.index(0).clone().label)
+                .on_input(|c| Message::ChangeButtons("label_one".to_string(), c)),
+            // Start RPC
+            button(text("Start Rich Presence")).on_press(Message::Start),
         ];
 
-        container(column).into()
+        container(column).center(Length::Fill).into()
     }
     fn update(&mut self, message: Message) {
         match message {
@@ -120,6 +146,21 @@ impl App {
                 "large_text" => self.assets.large_text = input.to_string(),
                 "small_image" => self.assets.small_image = input.to_string(),
                 "large_image" => self.assets.large_image = input.to_string(),
+                _ => unreachable!(),
+            },
+            Message::ChangeButtons(button, input) => match button {
+                val if val == "label_one".to_string() => {
+                    self.buttons.index_mut(0).label = input.to_string()
+                }
+                val if val == "url_one".to_string() => {
+                    self.buttons.index_mut(0).url = input.to_string()
+                }
+                val if val == "label_two".to_string() => {
+                    self.buttons.index_mut(1).label = input.to_string()
+                }
+                val if val == "large_image".to_string() => {
+                    self.buttons.index_mut(1).url = input.to_string()
+                }
                 _ => unreachable!(),
             },
             Message::Select(choice) => {
@@ -141,6 +182,12 @@ impl App {
                             .small_text(self.assets.small_text.as_mut_str())
                             .small_image(self.assets.small_image.as_mut_str()),
                     )
+                    .buttons(
+                        self.buttons
+                            .iter()
+                            .map(|b| Temp::new(b.label.as_str(), b.url.as_str()))
+                            .collect(),
+                    )
                     .activity_type(self.selected.clone().into());
                 let _ = client.as_mut().expect("").connect();
                 let _ = client.as_mut().expect("").set_activity(payload);
@@ -152,6 +199,7 @@ impl App {
 fn main() -> AppResult<()> {
     iced::application("presence", App::update, App::view)
         .theme(|_| Theme::Dark)
+        .window_size((400.0, 400.0))
         .run()?;
 
     // kill the presence when program is closed
